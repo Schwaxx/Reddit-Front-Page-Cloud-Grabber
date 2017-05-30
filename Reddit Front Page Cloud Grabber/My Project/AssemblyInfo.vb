b84f106b89283837dbd7a24882e92529da8515b6
SWB <SWB@681CA200AFDE>
2017-05-30 17:41:34 -0400

    Add project files.

---
+++ Reddit Front Page Cloud Grabber/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Reddit Front Page Cloud Grabber")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Reddit Front Page Cloud Grabber")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0cb66b87-7cd8-4f92-8f05-be0a2852770c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Reddit Front Page Cloud Grabber/Reddit Grabber.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Net
+Imports System.IO
+Public Class Main
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Date.Today > My.Settings.LastUpdate Then
+            Dim wrRequest As WebRequest
+            Dim wrResponse As String
+            Do Until wrResponse <> ""
+                Try
+                    wrRequest = HttpWebRequest.Create("https://www.reddit.com/")
+                    wrResponse = New StreamReader(wrRequest.GetResponse().GetResponseStream).ReadToEnd
+                Catch ex As Exception
+                    Threading.Thread.Sleep(3000)
+                End Try
+            Loop
+            Dim delim As String() = New String(0) {"data-outbound-expiration="}
+            Dim returnValue As String = vbNewLine
+            For i = 0 To wrResponse.Split(delim, StringSplitOptions.None).Count - 1
+                If Not i \ 2 = 0 Then
+                    Dim tempText As String = wrResponse.Split(delim, StringSplitOptions.None)(i)
+                    tempText = tempText.Substring(tempText.IndexOf(">") + 1, tempText.IndexOf("<") - tempText.IndexOf(">") - 1)
+                    returnValue += tempText & vbNewLine
+                End If
+            Next
+            My.Computer.FileSystem.WriteAllText("C:\Users\SWB\Documents\list.txt", returnValue, True)
+            My.Settings.LastUpdate = Date.Today
+            My.Settings.Save()
+        End If
+        Me.Close()
+    End Sub
+End Class
